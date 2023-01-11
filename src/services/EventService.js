b110dd7b5d959293c709f323400b600362a10c9c
Mike Erickson <mikerick@microsoft.com>
2023-01-11 14:46:17 -0700

    Created Event service for axios calls

---
+++ src/services/EventService.js
@@ -0,0 +1,17 @@
+import axios from "axios"
+
+const apiClient = axios.create({
+  baseURL:
+    "https://my-json-server.typicode.com/ericksoninco/mikes-real-world-3",
+  withCredentials: false,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+})
+
+export default {
+  getEvents() {
+    return apiClient.get("/events")
+  },
+}
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -8,7 +8,7 @@
 <script>
 // @ is an alias to /src
 import EventCard from "@/components/EventCard.vue"
-import axios from "axios"
+import EventService from "@/services/EventService.js"
 
 export default {
   name: "EventList",
@@ -21,10 +21,7 @@ export default {
     }
   },
   created() {
-    axios
-      .get(
-        "https://my-json-server.typicode.com/ericksoninco/my-real-world-3/events"
-      )
+    EventService.getEvents()
       .then((response) => {
         this.events = response.data
       })
